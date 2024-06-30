d3cd507d406df2e5f594e15c961cb85bd8c5d93a
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-06-30 01:45:16 -0600

    Adding initial implementation to listen to port

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import socketIOClient from 'socket.io-client';
+import PerformanceChart from './PerformanceChart';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const ENDPOINT = "http://localhost:4000";
+
+const App = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const socket = socketIOClient(ENDPOINT);
+        socket.on('performanceData', (newData) => {
+            setData(prevData => [...prevData, newData]);
+        });
+
+        return () => socket.disconnect();
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Browser Performance Monitor</h1>
+            <PerformanceChart data={data} />
+        </div>
+    );
+};
 
 export default App;
